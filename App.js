37001c459f8df1fa05bb7e56578b7989e1592cdd
Malek Ben Dali <bendali.malekk@gmail.com>
2021-05-18 14:32:53 +0100

    added google auth

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, Button} from 'react-native';
+// import AudioRecorderPlayer from 'react-native-audio-recorder-player';
+import auth from '@react-native-firebase/auth';
+import {GoogleSignin} from '@react-native-google-signin/google-signin';
+
+GoogleSignin.configure({
+  webClientId:
+    '452294585565-cma0bm3df6tbjeek76c4ead690pplhj3.apps.googleusercontent.com',
+});
 
 const App = () => {
+  async function onGoogleButtonPress() {
+    try {
+      // Get the users ID token
+      const {idToken} = await GoogleSignin.signIn();
+
+      // Create a Google credential with the token
+      const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+
+      // Sign-in the user with the credential
+      return auth().signInWithCredential(googleCredential);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  // const audioRecorderPlayer = new AudioRecorderPlayer();
+
   return (
-    <View>
-      <Text>test</Text>
-    </View>
+    <Button
+      title="Google Sign-In"
+      onPress={() =>
+        onGoogleButtonPress().then(() => console.log('Signed in with Google!'))
+      }
+    />
   );
 };
 
