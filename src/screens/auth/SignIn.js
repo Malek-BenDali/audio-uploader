8829f84e7396542666ac5f9a4caab9a77b0492a9
Malek Ben Dali <bendali.malekk@gmail.com>
2021-05-24 09:27:06 +0100

    deleted useless libraries

--- src/screens/auth/SignIn.js
+++ src/screens/auth/SignIn.js
@@ -4,31 +4,8 @@ import {googleSignIn} from '../../api/google/authApi';
 import {useDispatch, useSelector} from 'react-redux';
 import auth from '@react-native-firebase/auth';
 import {signIn} from '../../store/actions/authAction';
-import messaging from '@react-native-firebase/messaging';
-import database from '@react-native-firebase/database';
 
 const SignIn = () => {
-  useEffect(() => {
-    messaging().onMessage(async remoteMessage => {
-      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
-      console.log('A new FCM message arrived!', JSON.stringify(remoteMessage));
-    });
-
-    messaging().onNotificationOpenedApp(remoteMessage => {
-      console.log('onNotificationOpenedApp : ', JSON.stringify(remoteMessage));
-    });
-
-    messaging()
-      .getInitialNotification()
-      .then(remoteMessage => {
-        if (remoteMessage) {
-          console.log(
-            'Notification caused app to open from quit state:',
-            JSON.stringify(remoteMessage),
-          );
-        }
-      });
-  }, []);
   const dispatch = useDispatch();
 
   function onAuthStateChanged(user) {
@@ -45,13 +22,9 @@ const SignIn = () => {
   };
 
   return (
-    // <Button
-    //   title="Google Sign-In"
-    //   onPress={async () => handleGoogleSignIn().catch(err => console.log(err))}
-    // />
     <Button
       title="Google Sign-In"
-      onPress={async () => await handleGoogleSignIn()}
+      onPress={async () => handleGoogleSignIn().catch(err => console.log(err))}
     />
   );
   // loading ? (
